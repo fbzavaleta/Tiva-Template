577aa2aadae27442dcd7948882b53dd00a9f5650
fbzavaleta <benjamin1992.1992@gmail.com>
2023-08-29 19:59:07 +0100

    Using registers to blink the red led

--- Demo/blinky.c
+++ Demo/blinky.c
@@ -25,70 +25,23 @@
 #include <stdint.h>
 #include "inc/tm4c123gh6pm.h"
 
-//*****************************************************************************
-//
-//! \addtogroup example_list
-//! <h1>Blinky (blinky)</h1>
-//!
-//! A very simple example that blinks the on-board LED using direct register
-//! access.
-//
-//*****************************************************************************
+#define DELAY_VALUE        10000
 
-//*****************************************************************************
-//
-// Blink the on-board LED.
-//
-//*****************************************************************************
-int
-main(void)
+void delay (void)
 {
-    volatile uint32_t ui32Loop;
+    for(int i=0;i<DELAY_VALUE;i++);
+}
 
-    //
-    // Enable the GPIO port that is used for the on-board LED.
-    //
-    SYSCTL_RCGC2_R = SYSCTL_RCGC2_GPIOF;
-
-    //
-    // Do a dummy read to insert a few cycles after enabling the peripheral.
-    //
-    ui32Loop = SYSCTL_RCGC2_R;
-
-    //
-    // Enable the GPIO pin for the LED (PF3).  Set the direction as output, and
-    // enable the GPIO pin for digital function.
-    //
-    GPIO_PORTF_DIR_R = 0x08;
-    GPIO_PORTF_DEN_R = 0x08;
-
-    //
-    // Loop forever.
-    //
-    while(1)
-    {
-        //
-        // Turn on the LED.
-        //
-        GPIO_PORTF_DATA_R |= 0x08;
-
-        //
-        // Delay for a bit.
-        //
-        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++)
-        {
-        }
-
-        //
-        // Turn off the LED.
-        //
-        GPIO_PORTF_DATA_R &= ~(0x08);
-
-        //
-        // Delay for a bit.
-        //
-        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++)
-        {
-        }
-    }
+int main ( void )
+{
+SYSCTL_RCGCGPIO_R |= 0x20; // Enable clock for PORTF
+GPIO_PORTF_DEN_R  = 0x0E;  // Enable PORTF Pin1, 2 and 3 as a digital pins
+GPIO_PORTF_DIR_R  = 0x0E;  // Configure ORTF Pin1, 2 and 3 digital output pins
+		
+	while (1) 
+		{  
+		GPIO_PORTF_DATA_R |= 0x02; // turn on red LED
+        delay();
+        GPIO_PORTF_DATA_R &= 0x00; // turn off red LED
+		}
 }
